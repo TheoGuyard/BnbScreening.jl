bda9c15b765fc0f20201b0b965e5cf368a4c0369
Theo Guyard <theo.guyard@insa-rennes.fr>
2022-12-15 16:15:35 +0100

    Fix x size

--- src/bvls.jl
+++ src/bvls.jl
@@ -18,17 +18,17 @@ function solve_heuristic_bvls(
     )
 
     AS1 = A[:,node.branch.S1]
-    n = size(AS1)[2]
-    lb = fill(-M,(n,))
-    ub = fill(M,(n,))
+    nS1 = size(AS1)[2]
+    lb = fill(-M,(nS1,))
+    ub = fill(M,(nS1,))
 
-    x = zeros(Float64,n)
-    on_bound = zeros(Int,n)
+    xS1 = zeros(Float64,nS1)
+    on_bound = zeros(Int,nS1)
     free_set = (on_bound .== 0)
     active_set = .!free_set
     free_set = findall(free_set)
 
-    r = AS1 * x - y
+    r = AS1 * xS1 - y
     cost = r' * r
     g = AS1' * r
     cost_change = Inf
@@ -38,7 +38,7 @@ function solve_heuristic_bvls(
     while !isempty(free_set)
 
         AS1_free = AS1[:, free_set]
-        y_free = y - AS1 * (x .* active_set)
+        y_free = y - AS1 * (xS1 .* active_set)
         z = AS1_free \ y_free
         lbv = (z .< lb[free_set])
         ubv = (z .> ub[free_set])
@@ -46,20 +46,20 @@ function solve_heuristic_bvls(
 
         if any(lbv)
             ind = free_set[lbv]
-            x[ind] = lb[ind]
+            xS1[ind] = lb[ind]
             active_set[ind] .= true
             on_bound[ind] .= -1
         end
         if any(ubv)
             ind = free_set[ubv]
-            x[ind] = ub[ind]
+            xS1[ind] = ub[ind]
             active_set[ind] .= true
             on_bound[ind] .= 1
         end
 
         ind = free_set[.!v]
-        x[ind] = z[.!v]
-        r = AS1 * x - y
+        xS1[ind] = z[.!v]
+        r = AS1 * xS1 - y
         g = AS1' * r
         any(v) || break
         free_set = free_set[.!v]
@@ -80,11 +80,11 @@ function solve_heuristic_bvls(
             free_set = (on_bound .== 0)
             active_set = .!free_set
             free_set = findall(free_set)
-            x_free = x[free_set]
+            x_free = xS1[free_set]
             lb_free = lb[free_set]
             ub_free = ub[free_set]
             AS1_free = AS1[:,free_set]
-            y_free = y - AS1 * (x .* active_set)
+            y_free = y - AS1 * (xS1 .* active_set)
             z = AS1_free \ y_free
             lbv = findall(z .< lb_free)
             ubv = findall(z .> ub_free)
@@ -101,7 +101,7 @@ function solve_heuristic_bvls(
                 alpha  = alphas[i]
                 x_free *= 1. - alpha
                 x_free += alpha * z
-                x[free_set] = x_free
+                xS1[free_set] = x_free
 
                 if i < length(lbv)
                     on_bound[free_set[i_free]] = -1
@@ -110,12 +110,12 @@ function solve_heuristic_bvls(
                 end
             else
                 x_free = z
-                x[free_set] = x_free
+                xS1[free_set] = x_free
                 break
             end
         end
 
-        r = AS1 * x - y
+        r = AS1 * xS1 - y
         cost_new = 0.5 * (r' * r)
         cost_change = cost - cost_new
 
@@ -126,7 +126,9 @@ function solve_heuristic_bvls(
         optimality = compute_kkt_optimality(g,on_bound)
     end
 
-    obj = 0.5 * norm(r,2)^2 + λ * n
+    x = zeros(size(A)[2])
+    x[node.branch.S1] = xS1
+    obj = 0.5 * norm(r,2)^2 + λ * nS1
     
     return obj, x
 end
